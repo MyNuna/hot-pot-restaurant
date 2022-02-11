9a07e07434af0a09c065ba62fd8647744fa20c94
huhanlei <1179091911@qq.com>
2022-02-11 17:23:53 +0800

    2022/02/11 17:23

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { user } from './modules/user.js'
+import { app } from './modules/app.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        count: 1,
+    },
+    mutations: {
+        addmu(state) { state.count++ },
+        lessmu(state) { state.count-- },
+    },
+    actions: {},
+    modules: {
+        // user,
+        // app,
+    }
 })
