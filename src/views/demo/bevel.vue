ad6cdec7163eee901c2046ae711ff1df188ca561
haha <1179091911@qq.com>
2022-03-28 19:02:44 +0800

    2022/03/28 19:02

--- src/views/demo/bevel.vue
+++ src/views/demo/bevel.vue
@@ -4,14 +4,35 @@
            <div class="left-bottom">左下角</div>
            <div class="card-right">
                <h3>css</h3>
-               <span class="right-text">{{"background: linear-gradient(45deg, transparent 15px, pink 0);"}}</span>
+               <span>background: linear-gradient(45deg, transparent 15px, pink 0);</span>
            </div>
        </div>
        <div class="page-card">
            <div class="left-right-bottom">左下角和右下角</div>
            <div class="card-right">
                <h3>css</h3>
-               <span class="right-text">{{"background: linear-gradient(45deg, transparent 15px, pink 0);"}}</span>
+               <div>background: linear-gradient(45deg, transparent 15px, pink 0) left,</div>
+               <!-- &emsp;占两个字符一个汉字的长度，&nbsp;占一个字符的长度 -->
+               <div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;linear-gradient(-45deg, transparent 15px, green 0) right;</div> 
+               <div>background-size: 50% 100%; 背景大小，前一个参数是宽度，后一个参数是高度</div> 
+               <div>background-repeat: no-repeat; 防止背景重复加载</div> 
+           </div>
+       </div>
+       <div class="page-card">
+           <div class="top-bottom-left-right">四个外角</div>
+           <div class="card-right">
+               <h3>css</h3>
+               <div>background: linear-gradient(135deg, transparent 15px, pink 0) top left,</div>
+               <div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;linear-gradient(-135deg, transparent 15px, pink 0) top right,</div> 
+               <div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;linear-gradient(45deg, transparent 15px, pink 0) bottom left,</div> 
+               <div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;linear-gradient(-45deg, transparent 15px, pink 0) bottom right;</div> 
+           </div>
+       </div>
+       <div class="page-card">
+           <div class="filleting">内圆角</div>
+           <div class="card-right">
+               <h3>css</h3>
+               <div></div>
            </div>
        </div>
    </div>
@@ -42,17 +63,32 @@ methods: {},
 <style lang='scss' scoped>
 /* @import url(); 引入css类 */
 .page{}
+.filleting {
+    width: 100px;
+    height: 100px;
+}
+.top-bottom-left-right {
+    width: 100px;
+    height: 100px;
+    background: linear-gradient(135deg, transparent 15px, pink 0) top left,
+                linear-gradient(-135deg, transparent 15px, pink 0) top right,
+                linear-gradient(45deg, transparent 15px, pink 0) bottom left,
+                linear-gradient(-45deg, transparent 15px, pink 0) bottom right;
+    background-size: 50% 50%;
+    background-repeat: no-repeat;
+}
 .left-right-bottom {
     width: 100px;
     height: 100px;
-    background: linear-gradient(45deg, transparent 15px, pink 0),
-                linear-gradient(-45deg, transparent 15px, green 0);
-    background-size: 50% 100%;
+    background: linear-gradient(45deg, transparent 15px, pink 0) left,
+                linear-gradient(-45deg, transparent 15px, green 0) right;
+    background-size: 50% 100%; // 背景大小，前一个参数是宽度，后一个参数是高度
+    // background-repeat: no-repeat; // 防止背景重复加载
 }
 .left-bottom {
     width: 100px;
     height: 100px;
-    background: linear-gradient(45deg, transparent 15px, pink 0);
+    background: linear-gradient(45deg, transparent 15px, pink 0) ;
 }
 .page-card {
     display: flex;
@@ -64,7 +100,7 @@ methods: {},
     padding: 20px;
     
     .card-right {
-        padding: 0 20px;
+        padding: 0 20px;;
     }
 }
 </style>
